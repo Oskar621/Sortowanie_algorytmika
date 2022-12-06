b6fb1ca360380676165a6e913c42815a2c8872fa
User <asd>
2022-12-06 23:04:25 +0100

    ciag dalszy trop blizszy do rozwiazania zagwozdki z czasem

--- app/src/main/java/com/example/sortowanka/MainActivity.kt
+++ app/src/main/java/com/example/sortowanka/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.sortowanka
 
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,24 +32,36 @@ class MainActivity : AppCompatActivity() {
             //czyszczenie listy
             liczby.clear()
             //losowanie liczb do listy
-            if(iloscElementow.isNotEmpty() && iloscRazy.isNotEmpty()) {
-                if(iloscElementow.toString().toInt() >= 1)
-                for (i in 1..iloscElementow.toString().toInt())
-                    liczby.add(Random.nextInt(1,50))
+            if (iloscElementow.isNotEmpty() && iloscRazy.isNotEmpty()) {
+                if (iloscElementow.toString().toInt() >= 1)
+                    for (i in 1..iloscElementow.toString().toInt())
+                        liczby.add(Random.nextInt(1, 50))
 
-            //sortowanie przez wstawianie
-            for(i in 1 .. liczby.size+1)
-            {
-                val pom = liczby[i]
-                var j = i-1
-                while (j>=0 && liczby[j]>pom)
+                //sortowanie przez wstawianie
+                val c1: Calendar = Calendar.getInstance()
+                c1.set(Calendar.MONTH, 11)
+                c1.set(Calendar.DATE, 5)
+                c1.set(Calendar.YEAR, 1996)
+                val dateOne: Date = c1.getTime()
+                var tablica: MutableList<Int> = mutableListOf()
+                for(r in 1 ..iloscRazy.toString().toInt())
                 {
-                    liczby[j+1] = liczby[j]
-                    j-=1
+                    for(j in tablica.size-2 downTo 0)
+                    {
+                        val x = tablica[j]
+                        var i = j + 1
+                        while ((i<tablica.size)&&(x>tablica[i]))
+                        {
+                            tablica[i-1] = tablica[i]
+                            i+=1
+                        }
+                        tablica[i-1] = x
+                    }
+                    tablica = liczby
                 }
-                liczby[j+1] = pom
-            }
-                Toast.makeText(applicationContext, liczby.toString(), Toast.LENGTH_SHORT).show()
+                val dateTwo: Date = c1.getTime()
+                val wynik: Int = dateTwo.toString().toInt() - dateOne.toString().toInt()
+                czasWstawianie.text = wynik.toString()
             }
         }
     }
